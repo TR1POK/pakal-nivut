d9993f2cdf397daa8da641c48deee60b02120e05
TR1POK <90261163+TR1POK@users.noreply.github.com>
2025-08-08 00:53:20 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("intro-screen").classList.remove("hidden");
-  setTimeout(() => {
-    document.getElementById("intro-screen").classList.add("hidden");
-    document.getElementById("main-app").classList.remove("hidden");
-  }, 3000);
-});
